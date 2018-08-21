14406f443487476c187886fae95d560d12f54dbc
tormobr <tormobr@student.matnat.uio.no>
2018-08-21 21:37:03 +0200

    ignore file added

--- converter/Disp.java
+++ converter/Disp.java
@@ -7,11 +7,11 @@ class Disp extends JFrame{
 	private JLabel item1;
 	private JLabel item2;
 	private JLabel item3;
-	private JButton item4;
-	private JTextField item5;
-	private JTextField item6;
-	JTextField item7;
-	JTextField item8;
+	private JButton convertButton;
+	private JTextField number;
+	private JTextField binResult;
+	JTextField hexResult;
+	JTextField octResult;
 
 	public Disp(){
 		super("simple converter");
@@ -24,12 +24,12 @@ class Disp extends JFrame{
 		item1 = new JLabel("bin: ");
 		item2 = new JLabel("hex: ");
 		item3 = new JLabel("oct: ");
-		item4 = new JButton("Convert!");
-		item4.addActionListener(new Action());
-		item5 = new JTextField(10);
-		item6 = new JTextField(10);
-		item7 = new JTextField(10);
-		item8 = new JTextField(10);
+		convertButton = new JButton("Convert!");
+		convertButton.addActionListener(new Action());
+		number = new JTextField(10);
+		binResult = new JTextField(10);
+		hexResult = new JTextField(10);
+		octResult = new JTextField(10);
 
 
 
@@ -37,12 +37,12 @@ class Disp extends JFrame{
 		c.gridx = 0;
 		c.gridy = 0;
 
-		panel.add(item5, c);
+		panel.add(number, c);
 
 		c.gridx = 1;
 		c.gridy = 0;
 
-		panel.add(item4, c);
+		panel.add(convertButton, c);
 
 
 		c.gridx = 0;
@@ -62,26 +62,26 @@ class Disp extends JFrame{
 
 		c.gridx = 1;
 		c.gridy = 1;
-		panel.add(item6, c);
+		panel.add(binResult, c);
 
 		c.gridx = 1;
 		c.gridy = 2;
-		panel.add(item7, c);
+		panel.add(hexResult, c);
 
 		c.gridx = 1;
 		c.gridy = 3;
-		panel.add(item8, c);
+		panel.add(octResult, c);
 
 	}
 	class Action implements ActionListener{
 		public void actionPerformed(ActionEvent evt){
-			String hax = item5.getText();
+			String hax = number.getText();
 			String binary = converter.decToBin((Integer.parseInt(hax)));
 			String hexa = converter.decToHex((Integer.parseInt(hax)));
 			String octa = converter.decToOct((Integer.parseInt(hax)));
-			item6.setText(binary);
-			item7.setText(hexa);
-			item8.setText(octa);
+			binResult.setText(binary);
+			hexResult.setText(hexa);
+			octResult.setText(octa);
 		}
 	}
 
