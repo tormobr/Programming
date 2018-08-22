c8285a5c0ccbe2baffa3071630b3f46b1883a562
tormobr <tormobr@student.matnat.uio.no>
2018-08-22 19:27:28 +0200

    Added typetest

---
+++ type_test/TypeTest.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Scanner;
+
+class TypeTest{
+	public static void main(String[] args){
+        JFrame hax = new JFrame("Simple TypeTest.");
+        hax.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        hax.setSize(400, 300);
+        hax.setResizable(false);
+        hax.setVisible(true);
+	}
+}
